8e78f3699d8727a8c65829385422756c3ae70200
gnsamine <amine0gunes@gmail.com>
2023-08-25 14:44:24 +0300

    skeleton edited

--- skeleton/skeleton.go
+++ skeleton/skeleton.go
@@ -6,6 +6,10 @@ type Todo_list struct {
 	ID           uint `gorm:"primaryKey"`
 	Subject      string
 	Duty         string
-	Completed    bool
+	Completed    bool `gorm:"default:false"`
 	CreationTime time.Time
+}
+
+type Listtodo struct {
+	Alltodos []Todo_list
 }
