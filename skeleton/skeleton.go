a92ea26da3b963b763d1297ce45458d9ebdf2451
gnsamine <amine0gunes@gmail.com>
2023-09-07 16:14:48 +0300

    skeleton edited

--- skeleton/skeleton.go
+++ skeleton/skeleton.go
@@ -3,9 +3,9 @@ package skeleton
 import "time"
 
 type Todo_table struct {
-	ID           string    `json:"id"`
+	ID           string    `gorm:"primary key; autoIncrement" json:"id"`
 	Subject      string    `json:"subject"`
 	Duty         string    `json:"duty"`
-	Completed    bool      `gorm:"default:false"`
+	Completed    bool      `gorm:"default:false json:completed"`
 	CreationTime time.Time `json:"creationTime"`
 }
